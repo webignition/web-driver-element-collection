fb468b21040dc9c2795642c2749955b74e655fb2
Jon Cram <webignition@gmail.com>
2019-09-02 15:17:58 +0100

    Create collection (#4)

---
+++ src/WebDriverElementCollection.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace webignition\WebDriverElementCollection;
+
+use Facebook\WebDriver\WebDriverElement;
+
+class WebDriverElementCollection implements \Countable, \Iterator
+{
+    /**
+     * @var WebDriverElement[]
+     */
+    private $elements = [];
+    private $iteratorPosition = 0;
+
+    public function __construct(array $elements = [])
+    {
+        foreach ($elements as $element) {
+            if ($element instanceof WebDriverElement) {
+                $this->elements[] = $element;
+            }
+        }
+    }
+
+    public function get(int $index): ?WebDriverElement
+    {
+        return $this->elements[$index] ?? null;
+    }
+
+    // Countable methods
+
+    public function count(): int
+    {
+        return count($this->elements);
+    }
+
+    // Iterator methods
+
+    public function rewind()
+    {
+        $this->iteratorPosition = 0;
+    }
+
+    public function current(): WebDriverElement
+    {
+        return $this->elements[$this->iteratorPosition];
+    }
+
+    public function key(): int
+    {
+        return $this->iteratorPosition;
+    }
+
+    public function next()
+    {
+        ++$this->iteratorPosition;
+    }
+
+    public function valid(): bool
+    {
+        return isset($this->elements[$this->iteratorPosition]);
+    }
+}
---
+++ tests/WebDriverElementCollectionTest.php
@@ -0,0 +1,55 @@
+<?php
+/** @noinspection PhpUnhandledExceptionInspection */
+
+namespace webignition\WebDriverElementCollection\Tests;
+
+use Facebook\WebDriver\WebDriverElement;
+use webignition\WebDriverElementCollection\WebDriverElementCollection;
+
+class WebDriverElementCollectionTest extends \PHPUnit\Framework\TestCase
+{
+    public function testCountable()
+    {
+        $this->assertCount(0, new WebDriverElementCollection());
+
+        $this->assertCount(1, new WebDriverElementCollection([
+            \Mockery::mock(WebDriverElement::class),
+        ]));
+
+        $this->assertCount(2, new WebDriverElementCollection([
+            \Mockery::mock(WebDriverElement::class),
+            \Mockery::mock(WebDriverElement::class),
+        ]));
+    }
+
+    public function testIterator()
+    {
+        $elements = [
+            \Mockery::mock(WebDriverElement::class),
+            \Mockery::mock(WebDriverElement::class),
+            \Mockery::mock(WebDriverElement::class),
+        ];
+
+        $collection = new WebDriverElementCollection($elements);
+
+        foreach ($collection as $index => $element) {
+            $this->assertSame($elements[$index], $element);
+        }
+    }
+
+    public function testGet()
+    {
+        $elements = [
+            \Mockery::mock(WebDriverElement::class),
+            \Mockery::mock(WebDriverElement::class),
+            \Mockery::mock(WebDriverElement::class),
+        ];
+
+        $collection = new WebDriverElementCollection($elements);
+
+        $this->assertSame($elements[0], $collection->get(0));
+        $this->assertSame($elements[1], $collection->get(1));
+        $this->assertSame($elements[2], $collection->get(2));
+        $this->assertNull($collection->get(4));
+    }
+}
